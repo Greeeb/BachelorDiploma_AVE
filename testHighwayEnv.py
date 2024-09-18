c33e39dd33e12eabe8e5d5a8aafd7bd1f1b31010
Daniil Navodey <mementomoriden@gmail.com>
2024-09-18 11:31:46 +0200

    modified:   testHighwayEnv.py

--- testHighwayEnv.py
+++ testHighwayEnv.py
@@ -1,8 +1,8 @@
 import gymnasium
-import highway_env
+import highway_env  # required to build custom highway environment
 import random
 from matplotlib import pyplot as plt
-# %matplotlib inline
+from stable_baselines3 import SAC
 
 # Parameters to change in the environment configuration
 CONFIG_CHANGE = [
@@ -20,7 +20,7 @@ def setup_environment(config_change):
                          render_mode="rgb_array", 
                          config={
                              "action": {
-                                 "type": "DiscreteMetaAction" # "ContinuousAction"
+                                 "type": "ContinuousAction" # "DiscreteMetaAction" # 
                              }
                          })
     env.reset()
@@ -49,12 +49,24 @@ def find_action(obs, reward, done, trucated, info):
 
 
 def main():
+    # Setup the environment
     env = setup_environment(config_change=CONFIG_CHANGE)
     env.reset()
+
+    model = SAC("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=100, log_interval=10)
+    model.save("sac_pendulum")
+
+    del model # remove to demonstrate saving and loading
+
+    model = SAC.load("sac_pendulum")
+
+
     obs, reward, done, trucated, info = ([], 0, False, False, {})
-    for _ in range(30):
+    for _ in range(1000):
         # action = env.unwrapped.action_type.actions_indexes["IDLE"]
-        action = find_action(obs, reward, done, trucated, info)
+        # action = find_action(obs, reward, done, trucated, info)
+        action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, trucated, info = env.step(action=action)
         
         print(obs)
@@ -62,7 +74,7 @@ def main():
         # Stopping the environment if collision occured 
         # or the object is out of bounds/time limit is reached
         if done or trucated:
-            break
+            bobs, info = env.reset()
 
         env.render()
 
